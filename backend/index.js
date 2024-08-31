f10696d182d66ddf145ee07cbf5a536ac76e39b5
juanrealpe972 <juanrealpe972@gmail.com>
2024-08-31 01:39:58 -0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is running on port ${PORT}`);
+})
